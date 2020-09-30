1dde3404e278be146858f7443887332ca6e92ec8
Jacob.C <sbjcho@gmail.com>
2020-09-29 20:17:12 -0400

    Optional part3 of Module4 completed

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,6 +1,8 @@
 (function(){
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+  console.log("\n******** Part1 **********");
+
   for (var i = 0; i < names.length; i++) {
 
     var firstLetter = names[i].charAt(0).toLowerCase();
@@ -12,11 +14,36 @@
     }
   }
 
-  names.map(name => {
-    if(name.charAt(0).toLowerCase() == 'j'){
+  console.log("\n******** Part2 **********");
+
+  var greetingsMaker = function(name){
+    if(name.charAt(0).toLowerCase() === 'j'){
       console.log(byeSpeaker.speakSimple(name));
     } else {
       console.log(helloSpeaker.speakSimple(name));
     }
-  })
+  }
+
+  var mapGreetings = names.map(greetingsMaker)
+
+  console.log("\n******** Part3 (Optional) **********");
+
+  var initialValue = {hello: [], bye: []};
+  var reducer = function(accumulator, currentValue){
+      accumulator.hello.push(helloSpeaker.speakSimple(currentValue));
+      accumulator.bye.push(byeSpeaker.speakSimple(currentValue));
+
+      return accumulator;
+  }
+
+  const reduceGreetings = names.reduce(reducer, initialValue);
+
+  for (var i = 0; i < reduceGreetings.hello.length; i++){
+    console.log(reduceGreetings.hello[i]);
+  }
+
+  for (var i = 0; i < reduceGreetings.bye.length; i++){
+    console.log(reduceGreetings.bye[i]);
+  }
+  
 })();
